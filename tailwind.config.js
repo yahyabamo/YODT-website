985f17ac5ab37012ec691990ef9f8901109352f7
yahya <ya7yamonem777@gmail.com>
2026-04-24 20:49:50 +0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
+
   prefix: "",
   theme: {
     container: {
@@ -135,5 +136,6 @@ module.exports = {
       },
     },
   },
+
   plugins: [require("tailwindcss-animate")],
 };
